f5d00cb2f35d5f7616ff3780e7614c385a2070ee
djalehambode <himaachanamoussa@gmail.com>
2024-06-26 00:51:55 +0100

    2eme commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express()
 const port = 3000
 
 app.listen(port, ()=>{
-    console.log("Tout va bien !")
+    console.log("Tout va bien ! Ecoute sur le port"+port)
 })
 app.get('/', (req))
